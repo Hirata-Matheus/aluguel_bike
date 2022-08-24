b18ddb89143459b5ac790b1cff3ac0c17f9b0a96
Matheus Hirata <matheushirata2001@outlook.com>
2022-08-23 21:58:52 -0300

    Start no projeto

---
+++ app/src/main/java/com/example/aluguelbicicleta/model/Bicicleta.java
@@ -0,0 +1,10 @@
+package com.example.aluguelbicicleta.model;
+
+public class Bicicleta {
+    private Long id;
+    private String modelo;
+    private Long aro;
+    private String estado;
+    private String categoria;
+    private String descricao;
+}
