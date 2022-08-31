d67e050e9cbec33316129ac68723a08143b7bb96
Matheus Hirata <matheushirata2001@outlook.com>
2022-08-30 21:04:35 -0300

    Primeiras etapas

---
+++ app/src/main/java/com/example/aluguelbicicleta/activity/ListaBicicletasActivity.java
@@ -0,0 +1,23 @@
+package com.example.aluguelbicicleta.activity;
+
+import android.os.Bundle;
+import android.widget.ListView;
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.aluguelbicicleta.R;
+import com.example.aluguelbicicleta.model.Bicicleta;
+
+import java.util.List;
+
+public class ListaBicicletasActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_bikes);
+
+        //List<Bicicleta> bikes = todosAsBikes();
+
+        ListView listaBikes = (ListView) findViewById(R.id.lista);
+
+    }
+}
