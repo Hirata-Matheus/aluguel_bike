0d89302d95dd8c75877897fcf37bb17286cefe68
Matheus Hirata <matheushirata2001@outlook.com>
2022-09-20 21:03:56 -0300

    Criado tela principal de lista de bicicletas

--- app/src/main/java/com/example/aluguelbicicleta/MainActivity.java
+++ app/src/main/java/com/example/aluguelbicicleta/MainActivity.java
@@ -1,29 +1,70 @@
 package com.example.aluguelbicicleta;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.graphics.Typeface;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
+import com.example.aluguelbicicleta.activity.ListaBicicletasActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ListView listView;
+    private String modelosBikes[] = {
+            "Neo Sport",
+            "Bike Top das Galáxias",
+            "Bike da Avó",
+            "XTS Pro Bike",
+            "Caloi 304 XL"
+    };
+
+    private String categoriasBikes[] = {
+            "Sportiva",
+            "Lendária",
+            "Básica",
+            "Profissional",
+            "Intermediária"
+    };
+
+    private Integer fotosBikes[] = {
+            R.drawable.bike1,
+            R.drawable.bike2,
+            R.drawable.bike3,
+            R.drawable.bike3,
+            R.drawable.bike7
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_cadastro_pessoa);
+        setContentView(R.layout.activity_main);
 
-        Spinner spinner = findViewById(R.id.estado);
+        /*Spinner spinner = findViewById(R.id.estado);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
                 R.array.arrayEstado, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
-        spinner.setOnItemSelectedListener(this);
+        spinner.setOnItemSelectedListener(this);*/
+
+        TextView titulo = new TextView(this);
+        titulo.setTypeface(Typeface.DEFAULT_BOLD);
+        titulo.setText("Lista de Bicicletas");
+        titulo.setTextSize(30);
+        titulo.setPadding(0, 0, 0, 30);
+
+        ListView listView = (ListView) findViewById(R.id.lista);
+        listView.addHeaderView(titulo);
+
+        ListaBicicletasActivity customCountryList = new ListaBicicletasActivity(this, modelosBikes, categoriasBikes, fotosBikes);
+        listView.setAdapter(customCountryList);
+
+        listView.setOnItemClickListener((adapterView, view, position, l) -> Toast.makeText(getApplicationContext(),"Você Selecionou " + modelosBikes[position-1] + " o Modelo", Toast.LENGTH_SHORT).show());
     }
-    @Override
+    /*@Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
         String text = parent.getItemAtPosition(position).toString();
         Toast.makeText(parent.getContext(), text, Toast.LENGTH_SHORT).show();
@@ -32,5 +73,5 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     @Override
     public void onNothingSelected(AdapterView<?> parent) {
 
-    }
+    }*/
 }
--- app/src/main/java/com/example/aluguelbicicleta/activity/ListaBicicletasActivity.java
+++ app/src/main/java/com/example/aluguelbicicleta/activity/ListaBicicletasActivity.java
@@ -1,23 +1,41 @@
 package com.example.aluguelbicicleta.activity;
 
-import android.os.Bundle;
-import android.widget.ListView;
-import androidx.appcompat.app.AppCompatActivity;
+import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 import com.example.aluguelbicicleta.R;
-import com.example.aluguelbicicleta.model.Bicicleta;
 
-import java.util.List;
+public class ListaBicicletasActivity extends ArrayAdapter {
+    private String[] bikesModel;
+    private String[] categorias;
+    private Integer[] fotosBikes;
+    private Activity descricao;
 
-public class ListaBicicletasActivity extends AppCompatActivity {
+    public ListaBicicletasActivity(Activity descricao, String[] bikesModel, String[] categorias, Integer[] fotosBikes) {
+        super(descricao, R.layout.item_linha, bikesModel);
+        this.descricao = descricao;
+        this.bikesModel = bikesModel;
+        this.categorias = categorias;
+        this.fotosBikes = fotosBikes;
+    }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_lista_bikes);
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View row=convertView;
+        LayoutInflater inflater = descricao.getLayoutInflater();
+        if(convertView==null)
+        row = inflater.inflate(R.layout.item_linha, null, true);
+        TextView textViewCountry = (TextView) row.findViewById(R.id.txtViewModelo);
+        TextView textViewCapital = (TextView) row.findViewById(R.id.txtViewCategoria);
+        ImageView imageFlag = (ImageView) row.findViewById(R.id.fotoBikeFlag);
 
-        //List<Bicicleta> bikes = todosAsBikes();
-
-        ListView listaBikes = (ListView) findViewById(R.id.lista);
-
+        textViewCountry.setText(bikesModel[position]);
+        textViewCapital.setText(categorias[position]);
+        imageFlag.setImageResource(fotosBikes[position]);
+        return  row;
     }
 }
